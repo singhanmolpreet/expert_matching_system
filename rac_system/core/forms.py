70e072ac3c8605324d9a9c006e08943c4e32852d
Anmolpreet Singh <anmoljosan18@gmail.com>
2024-10-16 20:14:12 +0530

    Delete rac_system directory

--- rac_system/core/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UploadCSVForm(forms.Form):
-    expert_csv = forms.FileField(label="Upload Expert CSV")
-    candidate_csv = forms.FileField(label="Upload Candidate CSV")
